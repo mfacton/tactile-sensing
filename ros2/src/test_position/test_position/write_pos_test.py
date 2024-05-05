44ae10cdd4ce4c3aecf46fc380e6cf0ef6db8fe9
mfacton <mfacton1@gmail.com>
2024-05-05 16:38:05 -0400

    Add ros1 folder

--- ros2/src/test_position/test_position/write_pos_test.py
+++ ros2/src/test_position/test_position/write_pos_test.py
@@ -1,37 +1,35 @@
+import sys
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import PoseStamped
-import time
+from geometry_msgs.msg import Pose
+from moveit import MoveGroupInterface
 
-class CommandPublisher(Node):
+
+class MoveItNode(Node):
     def __init__(self):
-        super().__init__('command_publisher')
-        self.publisher = self.create_publisher(PoseStamped, '/forward_position_controller/commands', 10)
+        super().__init__('ur5_moveit_move')
+        self.move_group = MoveGroupInterface("ur_manipulator", "base_link")
 
-    def publish_command(self):
-        msg = PoseStamped()
-        msg.header.frame_id = ''  # Frame of reference for the pose
-        # Set the desired position and orientation
-        # [INFO] [1714849390.937519967] [tf_subscriber]: Current Position: [-0.49088613244258184, 0.10880145197764154, 0.37482623390297765]
-        # [INFO] [1714849390.938116668] [tf_subscriber]: Current Orientation: [-0.04526481894039408, -0.04281448841406358, -0.4978362267918881, 0.8650301191529409]
+    def move_to_position(self, target):
+        pose_target = Pose()
+        pose_target.orientation.w = 1.0
+        pose_target.position.x = target[0]
+        pose_target.position.y = target[1]
+        pose_target.position.z = target[2]
 
-        msg.pose.position.x = -0.4909
-        msg.pose.position.y = 0.1088
-        msg.pose.position.z = 0.3048
-        msg.pose.orientation.x = -0.04526481894039408
-        msg.pose.orientation.y = -0.04526481894039408
-        msg.pose.orientation.z = -0.4978362267918881
-        msg.pose.orientation.w = 0.8650301191529409  # Identity quaternion for orientation if 1.0
-        self.publisher.publish(msg)
+        self.move_group.moveToPose(pose_target, "tool0", 0.01)
+
 
 def main(args=None):
     rclpy.init(args=args)
-    command_publisher = CommandPublisher()
-    command_publisher.publish_command()  # Publish a single command
-    time.sleep(1)  # Delay for 1 second to allow publishing
-    rclpy.spin_once(command_publisher)  # Process any pending callbacks
-    command_publisher.destroy_node()
-    rclpy.shutdown()
+    moveit_node = MoveItNode()
+    try:
+        target_position = [0, 0, 0]  # XYZ position
+        moveit_node.move_to_position(target_position)
+    finally:
+        moveit_node.destroy_node()
+        rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
