8755078b198d70732c2e5f99839d86a72247ec64
mfacton <mfacton1@gmail.com>
2024-05-06 22:05:05 -0400

    Add read until function

--- tools/serial_manager.py
+++ tools/serial_manager.py
@@ -6,20 +6,27 @@ class DeviceNotFound(Exception):
 
 class SerialManager:
     """Wrapper class for pyserial"""
-    def __init__(self, device_name, baud=115200) -> None:
+    def __init__(self, device_name, baud=115200, device_number=1) -> None:
         self.baud = baud
         ports = serial.tools.list_ports.comports()
         for port in ports:
             if port.description == device_name:
-                self.device = port.device
-                self.ser = serial.Serial(self.device, self.baud)
-                return
+                if device_number == 1:
+                    self.device = port.device
+                    self.ser = serial.Serial(self.device, self.baud)
+                    return
+                else:
+                    device_number -= 1
 
         raise DeviceNotFound(f"{device_name} not found")
 
     def read_bytes(self, length):
         """Waits for length bytes recieved"""
         return self.ser.read(length)
+
+    def read_until(self, sequence):
+        """Reads until specific sequence"""
+        return self.ser.read_until(expected=sequence)
 
     def read_line(self):
         """Read line from serial"""
