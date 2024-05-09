f2d3c1facf2604fe4af1b47d5f7a8bd2c49791a9
mfacton <mfacton1@gmail.com>
2024-05-08 20:43:12 -0400

    Finish plot logger

--- arduino/plot_log.py
+++ arduino/plot_log.py
@@ -1,10 +1,11 @@
 import sys
-sys.path.append('../tactile-sensing')
+sys.path.append('..')
 
 import struct
 import time
 from tools.serial_manager import SerialManager, DeviceNotFound
 from tools.plotter import Plot
+from tools.logger import Logger
 
 NUM_ADC = 9
 
@@ -20,7 +21,13 @@ while True:
         break
     device_num += 1
 
+if not device_num:
+    print("No devices found")
+    sys.exit(1)
+
 plotter = Plot(data_length=NUM_ADC*len(managers), height_scale=0.5, pixel_shift=3, title="Device Plot")
+logger = Logger("data/log1.csv")
+logger.open()
 
 while True:
     all_values = []
@@ -30,3 +37,4 @@ while True:
 
     time.sleep(0.001)
     plotter.push(all_values)
+    logger.push(all_values)
