9e719a18610204b5cee55e6fbf392a1995020f05
Miguel Flores-Acton <mfacton1@gmail.com>
2024-07-01 18:00:35 -0400

    Add all 12 channels

--- arduino/SensorReader/SensorReader.ino
+++ arduino/SensorReader/SensorReader.ino
@@ -1,8 +1,8 @@
-#define NUM_ADC 9
+#define NUM_ADC 12
 #define BAUD 115200
-#define DELAY_MS 10
+#define DELAY_MS 20
 
-const uint8_t pins[] = { A0, A1, A2, A3, A4, A5, A6, A7, A8 };
+const uint8_t pins[] = { A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11 };
 uint16_t vals[NUM_ADC];
 uint8_t buf[NUM_ADC*2+2] = {'\r'};
 
