dc5bb352ba8da1ea25732e66bba8eccac0f3b72e
Miguel Flores-Acton <mfacton1@gmail.com>
2024-08-07 15:04:27 -0400

    Add probe-eight support

--- script/probe_print.py
+++ script/probe_print.py
@@ -1,13 +1,18 @@
+import time
+
 from tools.serial_manager import SerialManager
 
 manager = SerialManager("probe-board", baud=460800)
+# manager = SerialManager("probe-eight", baud=460800)
 
+sensors = 8
 
 def read_data():
     pressures = []
     temperatures = []
-    data = manager.read_bytes(40)
-    for s in range(8):
+    data = manager.read_bytes(sensors*5)
+    # print(data)
+    for s in range(sensors):
         start_index = s * 5
         pressures.append(
             data[start_index + 2] << 16 | data[start_index + 1] << 8 | data[start_index]
@@ -20,5 +25,6 @@ def read_data():
 while True:
     pressures, temperatures = read_data()
     print("========")
-    for s in range(8):
+    for s in range(sensors):
         print(f"Sensor {s:2d}: {pressures[s]/40960:8f}, {temperatures[s]/100:4f}")
+    time.sleep(0.001)
