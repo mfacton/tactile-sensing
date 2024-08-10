55703a5e718247a4674b2fe669a2891310d3059c
Miguel Flores-Acton <mfacton1@gmail.com>
2024-08-10 19:14:00 -0400

    probe board stm readout

--- stm32/probe-board/App/Src/app.c
+++ stm32/probe-board/App/Src/app.c
@@ -2,15 +2,25 @@
 #include "usbd_cdc_if.h"
 #include "lps22hh.h"
 
+extern TIM_HandleTypeDef htim1;
+
 void App_Init(void) {
 	HAL_Delay(100);
 	Lps22hh_Init();
+	HAL_TIM_Base_Start_IT(&htim1);
 }
 
-void App_Update(void) {
+void app_process(void) {
 	Lps22hh_Update();
 	uint8_t send_buf[LPS22HH_BUF_SIZE];
 	Lps22hh_Data(send_buf);
 	CDC_Transmit_FS(send_buf, LPS22HH_BUF_SIZE);
-	HAL_Delay(5);
 }
+
+void App_Update(void) {
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim) {
+	app_process();
+}
+
