d20cecaea648a0d25226d36727a5d292b1eb8ac1
Miguel Flores-Acton <mfacton1@gmail.com>
2024-08-12 10:55:08 -0400

    Remove rtde folder

--- rtde/README.md
+++
@@ -1,2 +0,0 @@
-# Real Time Data Exchange for UR5 control
-[https://www.universal-robots.com/articles/ur/interface-communication/real-time-data-exchange-rtde-guide/](RTDE Guide)
--- rtde/control2.py
+++
@@ -1,4 +0,0 @@
-from rtde_control import RTDEControlInterface as RTDEControl
-
-rtde_c = RTDEControl("192.168.56.101")
-rtde_c.moveL([-0.143, 0.435, 0.20, -0.001, 3.12, 0.04], 0.5, 0.3)
